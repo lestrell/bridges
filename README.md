eae15a927c0fe17e5e49041b7219b02d02ee7d8a
Sean Gallagher <stgallag@gmail.com>
2013-09-09 15:56:55 -0400

    Prototype as displayed at meeting

---
+++ README.md
@@ -0,0 +1,5 @@
+----
+Example invocation (from .). Uses GeoJSON, but easily pluggable. The
+visualization is hardly anything yet, but could be extended.
+
+    bridges/bridges.py examples/stack.py
--- bridges/wrapper.py
+++ bridges/wrapper.py
@@ -5,7 +5,6 @@ class Stack(object):
     # Wrappers may be different in a queue, tree, heap, etc.
     def __init__(self, wrapper):
         self.stack = wrapper.Stack()
-        self.node = wrapper.Node
     
     @property
     def root(self):
@@ -18,8 +17,12 @@ def Sandbox(filename):
     # Very short since it's basically just python on python
     return imp.load_source('student', filename)
 
-class JNI(object):
-    def __init__(self):
-        # NOTE: not portable, can only be instantiated once (yet)
-        jpype.startJVM("/usr/lib/jvm/default-java/jre/lib/amd64/server/libjvm.so")
-        
+def JNI(filename):
+    # NOTE: not portable, and can only be run once (yet)
+    # see examples here http://iacobelli.cl/blog/?p=119
+    classpath = os.path.dirname(filename)
+    classname = os.path.splitext(os.path.basename(filename))[0].title()
+    jpype.startJVM("/usr/lib/jvm/default-java/jre/lib/amd64/server/libjvm.so",
+		   "-ea", "-Djava.class.path=%s" %classpath)
+    # -- incomplete -- returns a class rather than a namespace
+    return jpype.JClass(classname)
